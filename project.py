a567a9c0663d94b3372a05bdde9db2bd6d1d99df
Priyaduraisamy346 <priyaduraisamy346@gmail.com>
2024-07-18 10:40:11 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+#import flask
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "<div style='background:darkblue'><h1>Hello world</h1><br><h1>Hai</h1></div>"
+if __name__=='__main__':
+    app.run(debug=True)
+                        
+
